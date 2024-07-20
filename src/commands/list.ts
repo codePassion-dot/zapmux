c737e697256c9d8af04047b6d3a6ac68f118ed7b
Juan Jacobo Jaramillo <jjjaramilloar@unal.edu.co>
2024-07-19 20:29:11 -0500

    feat: when listing show which projects have already a tmux session

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,11 +1,19 @@
 import chalk from "chalk";
 import db from "../lib/db";
+import { execSync } from "child_process";
 
 export default async () => {
   const projectsNames = await db.readAll();
+  const runningSessions = execSync('tmux list-sessions -F "#S"')
+    .toString()
+    .split("\n");
   console.log(
     projectsNames
-      .map((projectName) => `${chalk.green("❯")} ${projectName}`)
+      .map((projectName) =>
+        runningSessions.includes(projectName)
+          ? `${chalk.green("❯")} ${projectName} ${chalk.green("(running)")}`
+          : `${chalk.green("❯")} ${projectName}`,
+      )
       .join("\n"),
   );
 };
